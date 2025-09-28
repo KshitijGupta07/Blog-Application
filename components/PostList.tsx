0067df6c9105ad2a63fd82e8ba1d5a5846732803
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-09-28 13:06:03 +0530

    Update PostList.tsx

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,14 +1,13 @@
 import PostCard from "./PostCard";
 
-export default function PostList({ posts }: { posts: any[] }) {
-  if (!posts?.length) {
-    return <p className="text-center text-muted">No posts yet.</p>;
-  }
+export default function PostList({ posts, currentUserId }: { posts: any[], currentUserId: string }) {
+  if (!posts?.length) return <p className="text-center text-muted">No posts yet.</p>;
+
   return (
     <div className="row g-3">
-      {posts.map((p) => (
-        <div key={String(p._id)} className="col-12 col-md-6 col-lg-4">
-          <PostCard post={p} />
+      {posts.map((post) => (
+        <div key={String(post._id)} className="col-12 col-md-6 col-lg-4">
+          <PostCard post={post} currentUserId={currentUserId} />
         </div>
       ))}
     </div>
