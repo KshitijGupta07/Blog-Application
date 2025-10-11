86061d0db8d1e678cd43d7787913701a2f78a834
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-10-11 14:05:44 +0530

    Update User.ts

--- models/User.ts
+++ models/User.ts
@@ -4,7 +4,7 @@ const UserSchema = new Schema(
   {
     name: String,
     email: { type: String, required: true, unique: true },
-    image: String,
+    
     // keep selectable so credentials login can read it when requested
     passwordHash: { type: String, select: false },
   },
